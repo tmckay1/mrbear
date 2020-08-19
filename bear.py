11cd44f2ee3434d6769caba01692b90127f8b9ab
Tyler McKay <tmckay1.jackson@gmail.com>
2020-08-18 23:45:37 -0400

    fix bug

--- bear.py
+++ bear.py
@@ -2,6 +2,7 @@ from ledController.BiblioPixelLedController import BiblioPixelLedController
 from colors.BiblioPixelColors import BiblioPixelColors
 
 from bibliopixel.drivers.PiWS281X import PiWS281X
+import time
 
 # init led controller
 numLeds        = 35   # number of ambient leds
@@ -10,10 +11,11 @@ colors         = BiblioPixelColors()
 driver         = PiWS281X(numLeds)
 ledController  = BiblioPixelLedController(colors, driver)
 ledController.turnOnColor("red")
-sleep(4)
+time.sleep(4)
 ledController.turnOff()
-sleep(4)
-ledController.turnOnColor("red")
-sleep(4)
+time.sleep(4)
+ledController.turnOnColor("blue")
+time.sleep(4)
+ledController.turnOff()
 
 # Create listener object that will listen for certain phrases
